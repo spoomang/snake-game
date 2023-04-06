17092eb97854ebbaf3ae4b36d035e160ae6bf17f
sandeep poomanglaath <sandeeppoomanglaath@sandeeps-MacBook-Pro.local>
2023-04-06 15:20:25 +0800

    adding server

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const {getContentType, getFilePath} = require('./utilities/utilities');
+
+const PORT = 8080;
+
+http.createServer((request, response) => {
+    const filePath = getFilePath(request.url);
+
+    fs.readFile(filePath, function(error, content) {
+        const contentType = getContentType(filePath);
+
+        response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+    });
+
+}).listen(PORT);
---
+++ server/utilities/utilities.js
@@ -0,0 +1,37 @@
+const path = require('path');
+
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    let contentType = 'text/html';
+
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+        }
+    return contentType;
+}
+
+function getFilePath(fileUrl) {
+    return "."+fileUrl;
+}
+
+module.exports = {
+    getContentType,
+    getFilePath,
+};
