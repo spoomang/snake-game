688481961bd2ec65accee1c0d97a22425f1193e6
sandeep poomanglaath <sandeeppoomanglaath@sandeeps-MacBook-Pro.local>
2023-04-08 09:41:44 +0800

    minor refactor

--- test/client/components/game.js
+++ test/client/components/game.js
@@ -3,11 +3,15 @@ const sinon = require('sinon')
 const Game = require('../../../client/components/game');
 
 describe('Game', function () {
+  afterEach(() => {
+  })
+
   describe('updateFood', function () {
     let contextStubDrawImage;
     let dummyContext = {
         drawImage: () => {},
     };
+
     beforeEach(() => {
         contextStubDrawImage = sinon.stub(dummyContext, 'drawImage');
     });
