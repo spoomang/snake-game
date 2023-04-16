3604452d7a3a913ea736fbee1d5361aa47c310db
sandeep poomanglaath <sandeeppoomanglaath@sandeeps-MacBook-Pro.local>
2023-04-16 14:29:28 +0800

    server refactor

--- server/server.js
+++ server/server.js
@@ -3,6 +3,7 @@ const fs = require('fs');
 const path = require('path');
 
 const {getContentType, getFilePath} = require('./utils/utils');
+const regex = require('./utils/regex');
 
 const PORT = 8080;
 
@@ -10,17 +11,22 @@ http.createServer((request, response) => {
     let filePath;
 
     const url = request.url;
-    if (url === '/') {
-        filePath = './dist/index.html';
-    } else {
-        filePath = getFilePath(url);
+
+
+    if (regex.isCodeResource(url) || url === '/') {
+        if (url === '/') {
+            filePath = './dist/index.html';
+        } else {
+            filePath = getFilePath(url);
+        }
+    
+        fs.readFile(filePath, function(error, content) {
+            const contentType = getContentType(filePath);
+    
+            response.writeHead(200, { 'Content-Type': contentType });
+                response.end(content, 'utf-8');
+        });
     }
 
-    fs.readFile(filePath, function(error, content) {
-        const contentType = getContentType(filePath);
-
-        response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-    });
 
 }).listen(PORT);
